cde0db7fc4d68bc96899aedafe5f916d1e5f784e
sean830314 <sean830314@gmail.com>
2019-12-23 19:56:30 +0800

    Commit auto create db schema

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-#import os
-
 from flask import Flask
 from flask_restful import Api
-#from flask_jwt import JWT
-
-#from security import authenticate, identity
 from resources.user import UserRegister
 from resources.article import Article, ArticleList
-
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://sean:sean830314@127.0.0.1:3306/flask-restful-example"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@127.0.0.1:3306/flask_demo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 api = Api(app)
@@ -19,4 +13,6 @@ api.add_resource(ArticleList, '/articles')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(port=5001, debug=True)
