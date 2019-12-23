038df472008f808bde638972f1800631744e3a7b
sean830314 <sean830314@gmail.com>
2019-12-23 19:58:53 +0800

    change content field 40 Varchar to 100 Varchar

--- models/article.py
+++ models/article.py
@@ -1,12 +1,14 @@
 from db import db
 import datetime
+
+
 class ArticleModel(db.Model):
     __tablename__ = 'article'
 
     aid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40))
     author = db.Column(db.String(40))
-    content = db.Column(db.String(40))
+    content = db.Column(db.String(100))
     ip = db.Column(db.String(40))
     submission_date = db.Column(db.Date, default=datetime.datetime.now())
 
@@ -22,10 +24,10 @@ class ArticleModel(db.Model):
         return {'aid': self.aid, 'title': self.title, 'author': self.author, 'content': self.content, 'ip': self.ip, 'submission_date': self.submission_date.strftime("%Y-%m-%d") }
 
     @classmethod
-    def find_by_aid(cls,aid):
+    def find_by_aid(cls, aid):
         return cls.query.filter_by(aid=aid).first()
 
-    def save_to_db(self): #upserting
+    def save_to_db(self):  # upserting
         db.session.add(self)
         db.session.commit()
 
